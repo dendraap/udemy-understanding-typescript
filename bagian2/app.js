53fd8bfb76f6544bac84bbce76433954e3beb233
Dendra Ardana Putra <dendraae1@gmail.com>
2024-08-04 19:21:59 +0700

    Using Types

---
+++ bagian2/app.js
@@ -0,0 +1,7 @@
+var count = function (n1, n2) {
+    return n1 + n2;
+};
+var a = 1;
+var b = 2.5;
+var result = count(a, b);
+console.log("🚀 ~ result:", result);
---
+++ bagian2/app.ts
@@ -0,0 +1,9 @@
+const count = (n1: number, n2: number) => {
+    return n1+n2
+}
+
+const a = 1
+const b = 2.5
+
+const result = count(a,b)
+console.log("🚀 ~ result:", result)
