51b7e211b7f0ed0996f16e7f13b1c753b7336d94
Dendra Ardana Putra <dendraae1@gmail.com>
2024-08-04 22:51:05 +0700

    Working with Numbers, Strings, & Booleans

--- bagian2/app.js
+++ bagian2/app.js
@@ -1,10 +1,13 @@
-var count = function (n1, n2) {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('Incorrect input data types!');
-    }
-    return n1 + n2;
+var count = function (n1, n2, showResult, resultPhrase) {
+    var result = n1 + n2;
+    return (typeof n1 !== 'number' || typeof n2 !== 'number')
+        ? (function () { throw new Error('Incorrect data types!'); })()
+        : showResult
+            ? (console.log(resultPhrase, result), undefined)
+            : result;
 };
 var a = 1;
 var b = 2.5;
-var result = count(a, b);
-console.log("🚀 ~ result:", result);
+var showResult = true;
+var resultPhrase = 'Result: ';
+var last_result = count(a, b, showResult, resultPhrase);
--- bagian2/app.ts
+++ bagian2/app.ts
@@ -1,10 +1,15 @@
-const count = (n1: number, n2: number): number => {
-    if(typeof n1 !== 'number' || typeof n2 !== 'number') throw new Error('Incorrect input data types!')
-    return n1+n2
-}
+const count = (n1: number, n2: number, showResult: boolean, resultPhrase: string): number | void => {
+    const result = n1+n2;
+    return (typeof n1 !== 'number' || typeof n2 !== 'number')
+        ? (() => {throw new Error('Incorrect data types!')}) ()
+        : showResult 
+            ? (console.log(resultPhrase, result), undefined) 
+            : result
+};
 
-const a = 1
-const b = 2.5
+const a = 1;
+const b = 2.5;
+const showResult = true;
+const resultPhrase = 'Result: ';
 
-const result = count(a,b)
-console.log("🚀 ~ result:", result)
+const last_result = count(a,b, showResult, resultPhrase);
