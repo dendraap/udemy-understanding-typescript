898360821955fc2c9788a9fa6b5ed4e32f79f001
Dendra Ardana Putra <dendraae1@gmail.com>
2024-08-05 18:02:40 +0700

    Object Types

--- bagian2/app.js
+++ bagian2/app.js
@@ -1,13 +1,5 @@
-var count = function (n1, n2, showResult, resultPhrase) {
-    var result = n1 + n2;
-    return (typeof n1 !== 'number' || typeof n2 !== 'number')
-        ? (function () { throw new Error('Incorrect data types!'); })()
-        : showResult
-            ? (console.log(resultPhrase, result), undefined)
-            : result;
+var person = {
+    name: 'Dana',
+    age: 21
 };
-var a = 1;
-var b = 2.5;
-var showResult = true;
-var resultPhrase = 'Result: ';
-var last_result = count(a, b, showResult, resultPhrase);
+console.log("🚀 ~ person.name:", person.name);
--- bagian2/app.ts
+++ bagian2/app.ts
@@ -1,15 +1,9 @@
-const count = (n1: number, n2: number, showResult: boolean, resultPhrase: string): number | void => {
-    const result = n1+n2;
-    return (typeof n1 !== 'number' || typeof n2 !== 'number')
-        ? (() => {throw new Error('Incorrect data types!')}) ()
-        : showResult 
-            ? (console.log(resultPhrase, result), undefined) 
-            : result
-};
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: 'Dana',
+    age: 21
+}
 
-const a = 1;
-const b = 2.5;
-const showResult = true;
-const resultPhrase = 'Result: ';
-
-const last_result = count(a,b, showResult, resultPhrase);
+console.log("🚀 ~ person.name:", person.name)
