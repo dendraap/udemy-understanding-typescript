c52ff9205ec18b7503541cee49baeb1fdea30c3b
Dendra Ardana Putra <dendraae1@gmail.com>
2024-08-07 23:02:06 +0700

    Type Aliases / Custom Types

--- bagian2/app.ts
+++ bagian2/app.ts
@@ -1,4 +1,7 @@
-const combine = (input1: number|string, input2: number|string, resultConversion: 'as_number' | 'as_string'): number|string => {
+type ConversionType = 'as_number' | 'as_string'
+type Combinable = number | string
+
+const combine = (input1: Combinable, input2: Combinable, resultConversion: ConversionType): number|string => {
     return (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as_number')
         ? +input1 + +input2
         : input1.toString() + ' ' + input2.toString()
