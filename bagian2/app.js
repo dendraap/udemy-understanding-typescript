ff58d195607379dab554562cd9474ef65bd547a1
Dendra Ardana Putra <dendraae1@gmail.com>
2024-08-07 23:26:19 +0700

    Type Aliases & Object Types

--- bagian2/app.js
+++ bagian2/app.js
@@ -1,7 +1,40 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var combine = function (input1, input2, resultConversion) {
     return (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as_number')
         ? +input1 + +input2
         : input1.toString() + ' ' + input2.toString();
+};
+var isOlderUser = function (user1, user2) {
+    return (user1.age > user2.age)
+        ? user1.name + ' is older ' + Math.abs(user1.age - user2.age) + ' year then ' + user2.name
+        : user2.name + ' is older ' + Math.abs(user1.age - user2.age) + ' year then ' + user1.name;
+};
+var u1 = {
+    name: 'Dana',
+    age: 21,
+    address: {
+        city: 'Juwana',
+        street: 'Jl. Kepodang',
+        postalcode: 59185
+    },
+    hobbies: ['Gaming', 'Badminton'],
+    role: Role.ADMIN
+};
+var u2 = {
+    name: 'Dana2',
+    age: 22,
+    address: {
+        city: 'Gresik',
+        street: 'Jl. Veteran Gg IX',
+        postalcode: 61122
+    },
+    hobbies: ['Makan', 'Tidur', 'Nonton Youtube'],
+    role: Role.AUTHOR
 };
 var combineNumber = combine(14, 23, 'as_number');
 console.log("🚀 ~ combineNumber:", combineNumber);
@@ -9,3 +42,6 @@ var combineMix = combine('14', '23', 'as_number');
 console.log("🚀 ~ combineMix:", combineMix);
 var combineString = combine('Dendra', 'Ardana', 'as_string');
 console.log("🚀 ~ combineString:", combineString);
+var checkUserAge = isOlderUser(u1, u2);
+console.log("🚀 ~ checkUserAge:", checkUserAge);
+console.log("🚀 ~ u1:", u1);
--- bagian2/app.ts
+++ bagian2/app.ts
@@ -1,10 +1,52 @@
+enum Role {ADMIN, READ_ONLY, AUTHOR}
+
 type ConversionType = 'as_number' | 'as_string'
 type Combinable = number | string
+type User = {
+    name: string
+    age: number
+    address: {
+        city: string
+        street: string
+        postalcode: number
+    }
+    hobbies: string[]
+    role: Role
+}
 
 const combine = (input1: Combinable, input2: Combinable, resultConversion: ConversionType): number|string => {
     return (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as_number')
         ? +input1 + +input2
         : input1.toString() + ' ' + input2.toString()
+}
+
+const isOlderUser = (user1: User, user2: User): string => {
+    return (user1.age > user2.age)
+        ? user1.name + ' is older ' + Math.abs(user1.age-user2.age) + ' year then ' + user2.name
+        : user2.name + ' is older ' + Math.abs(user1.age-user2.age) + ' year then ' + user1.name
+}
+
+const u1: User = {
+    name: 'Dana',
+    age: 21,
+    address: {
+        city: 'Juwana',
+        street: 'Jl. Kepodang',
+        postalcode: 59185
+    },
+    hobbies: ['Gaming', 'Badminton'],
+    role: Role.ADMIN
+}
+const u2: User = {
+    name: 'Dana2',
+    age: 22,
+    address: {
+        city: 'Gresik',
+        street: 'Jl. Veteran Gg IX',
+        postalcode: 61122
+    },
+    hobbies: ['Makan', 'Tidur', 'Nonton Youtube'],
+    role: Role.AUTHOR
 }
 
 const combineNumber = combine(14, 23, 'as_number')
@@ -15,3 +57,8 @@ console.log("🚀 ~ combineMix:", combineMix)
 
 const combineString = combine('Dendra', 'Ardana', 'as_string')
 console.log("🚀 ~ combineString:", combineString)
+
+const checkUserAge = isOlderUser(u1, u2)
+console.log("🚀 ~ checkUserAge:", checkUserAge)
+
+console.log("🚀 ~ u1:", u1)
