e4851c1758391da558ac6bd682aa82cb45057b42
Dendra Ardana Putra <dendraae1@gmail.com>
2024-08-07 22:43:39 +0700

    Literal Types

--- bagian2/app.js
+++ bagian2/app.js
@@ -1,9 +1,16 @@
-var combine = function (input1, input2) {
-    return (typeof input1 === 'number' && typeof input2 === 'number')
-        ? input1 + input2
-        : input1.toString() + input2.toString();
+var Type;
+(function (Type) {
+    Type["NUMBER"] = "number";
+    Type["STRING"] = "string";
+})(Type || (Type = {}));
+var combine = function (input1, input2, resultConversion) {
+    return (typeof input1 === Type.NUMBER && typeof input2 === Type.NUMBER || resultConversion === Type.NUMBER)
+        ? +input1 + +input2
+        : input1.toString() + ' ' + input2.toString();
 };
-var combineNumber = combine(14, 23);
+var combineNumber = combine(14, 23, Type.NUMBER);
 console.log("🚀 ~ combineNumber:", combineNumber);
-var combineString = combine('Dendra', 'Ardana');
+var combineMix = combine('14', '23', Type.NUMBER);
+console.log("🚀 ~ combineMix:", combineMix);
+var combineString = combine('Dendra', 'Ardana', Type.STRING);
 console.log("🚀 ~ combineString:", combineString);
--- bagian2/app.ts
+++ bagian2/app.ts
@@ -1,11 +1,16 @@
-const combine = (input1: number|string, input2: number|string): number|string => {
-    return (typeof input1 === 'number' && typeof input2 === 'number')
-        ? input1 + input2
-        : input1.toString() + input2.toString()
+enum Type {NUMBER = 'number', STRING= 'string'}
+
+const combine = (input1: number|string, input2: number|string, resultConversion: Type): number|string => {
+    return (typeof input1 === Type.NUMBER && typeof input2 === Type.NUMBER || resultConversion === Type.NUMBER)
+        ? +input1 + +input2
+        : input1.toString() + ' ' + input2.toString()
 }
 
-const combineNumber = combine(14,23)
+const combineNumber = combine(14, 23, Type.NUMBER)
 console.log("🚀 ~ combineNumber:", combineNumber)
 
-const combineString = combine('Dendra', 'Ardana')
+const combineMix = combine('14', '23', Type.NUMBER)
+console.log("🚀 ~ combineMix:", combineMix)
+
+const combineString = combine('Dendra', 'Ardana', Type.STRING)
 console.log("🚀 ~ combineString:", combineString)
