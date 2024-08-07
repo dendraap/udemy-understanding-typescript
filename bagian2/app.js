4301bcb17c3c6be4dfa96af2c2938d11d097ff38
Dendra Ardana Putra <dendraae1@gmail.com>
2024-08-07 22:50:19 +0700

    Literal Types

--- bagian2/app.js
+++ bagian2/app.js
@@ -1,16 +1,11 @@
-var Type;
-(function (Type) {
-    Type["NUMBER"] = "number";
-    Type["STRING"] = "string";
-})(Type || (Type = {}));
 var combine = function (input1, input2, resultConversion) {
-    return (typeof input1 === Type.NUMBER && typeof input2 === Type.NUMBER || resultConversion === Type.NUMBER)
+    return (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as_number')
         ? +input1 + +input2
         : input1.toString() + ' ' + input2.toString();
 };
-var combineNumber = combine(14, 23, Type.NUMBER);
+var combineNumber = combine(14, 23, 'as_number');
 console.log("🚀 ~ combineNumber:", combineNumber);
-var combineMix = combine('14', '23', Type.NUMBER);
+var combineMix = combine('14', '23', 'as_number');
 console.log("🚀 ~ combineMix:", combineMix);
-var combineString = combine('Dendra', 'Ardana', Type.STRING);
+var combineString = combine('Dendra', 'Ardana', 'as_string');
 console.log("🚀 ~ combineString:", combineString);
--- bagian2/app.ts
+++ bagian2/app.ts
@@ -1,16 +1,14 @@
-enum Type {NUMBER = 'number', STRING= 'string'}
-
-const combine = (input1: number|string, input2: number|string, resultConversion: Type): number|string => {
-    return (typeof input1 === Type.NUMBER && typeof input2 === Type.NUMBER || resultConversion === Type.NUMBER)
+const combine = (input1: number|string, input2: number|string, resultConversion: 'as_number' | 'as_string'): number|string => {
+    return (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as_number')
         ? +input1 + +input2
         : input1.toString() + ' ' + input2.toString()
 }
 
-const combineNumber = combine(14, 23, Type.NUMBER)
+const combineNumber = combine(14, 23, 'as_number')
 console.log("🚀 ~ combineNumber:", combineNumber)
 
-const combineMix = combine('14', '23', Type.NUMBER)
+const combineMix = combine('14', '23', 'as_number')
 console.log("🚀 ~ combineMix:", combineMix)
 
-const combineString = combine('Dendra', 'Ardana', Type.STRING)
+const combineString = combine('Dendra', 'Ardana', 'as_string')
 console.log("🚀 ~ combineString:", combineString)
