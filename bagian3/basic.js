7a517b98b00944959819f0e0d0e07b9a1482c4be
Dendra Ardana Putra <dendraae1@gmail.com>
2024-08-13 12:40:10 +0700

    Setting a Compilation Target

--- bagian3/basic.js
+++ bagian3/basic.js
@@ -3,4 +3,7 @@ console.log('Basic.ts');
 let age;
 age = 20;
 const address = "me";
+let street;
+street = 230;
+const phone = age + street;
 console.log("🚀 ~ age:", age);
--- bagian3/basic.ts
+++ bagian3/basic.ts
@@ -2,4 +2,7 @@ console.log('Basic.ts')
 let age: number
 age = 20
 const address: string = "me"
+let street: number
+street = 230
+const phone: number = age+street
 console.log("🚀 ~ age:", age)
