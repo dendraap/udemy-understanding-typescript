6c8b9e954ba5985dc18ed2383ad1df48a9875a38
Dendra Ardana Putra <dendraae1@gmail.com>
2024-08-14 15:17:59 +0700

    Strict Compilation & Code Quality Options

--- bagian3/dist/app.js
+++ bagian3/dist/app.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('This is app.ts');
+const btn1 = document.querySelector('button');
+const clicked = (message) => {
+    console.log(message);
+};
+const button = (message) => {
+    console.log(message);
+    return message;
+};
+if (btn1) {
+    btn1.addEventListener('click', () => clicked("Clicked"));
+}
--- bagian3/src/app.ts
+++ bagian3/src/app.ts
@@ -1,1 +1,14 @@
-console.log('This is app.ts')
+const btn1 = document.querySelector('button')!
+
+const clicked = (message: string) => {
+    console.log(message)
+}
+
+const button = (message: string): string => {
+    console.log(message)
+    return message
+}
+// do something when clicked
+if (btn1) {
+    btn1.addEventListener('click', () => clicked("Clicked"))
+}
